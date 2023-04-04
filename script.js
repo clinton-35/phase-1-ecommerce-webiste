8b897000716202056b8a159fa9e6474d9d8858c0
clinton <clinton.wambua@student.moringaschool.com>
2023-04-04 14:19:37 +0300

    updates

--- script.js
+++ script.js
@@ -132,3 +132,34 @@ function loadCart(){
     // calculate and update UI of cart info 
     updateCartInfo();
 }
+// calculate total price of the cart and other info
+function findCartInfo(){
+    let products = getProductFromStorage();
+    let total = products.reduce((acc, product) => {
+        let price = parseFloat(product.price.substr(1)); // removing dollar sign
+        return acc += price;
+    }, 0); // adding all the prices
+
+    return{
+        total: total.toFixed(2),
+        productCount: products.length
+    }
+}
+// delete product from cart list and local storage
+function deleteProduct(e){
+    let cartItem;
+    if(e.target.tagName === "BUTTON"){
+        cartItem = e.target.parentElement;
+        cartItem.remove(); // this removes from the DOM only
+    } else if(e.target.tagName === "I"){
+        cartItem = e.target.parentElement.parentElement;
+        cartItem.remove(); // this removes from the DOM only
+    }
+
+    let products = getProductFromStorage();
+    let updatedProducts = products.filter(product => {
+        return product.id !== parseInt(cartItem.dataset.id);
+    });
+    localStorage.setItem('products', JSON.stringify(updatedProducts)); // updating the product list after the deletion
+    updateCartInfo();
+}
